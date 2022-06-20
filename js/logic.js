f95209d1d214830969ebaca19673068a5285b842
kahlstorf1 <kahlstorf1@outlook.com>
2022-06-20 16:32:23 -0400

    One full round of RPS with user prompt input

--- js/logic.js
+++ js/logic.js
@@ -6,12 +6,51 @@ function computerLogic() {
 let x = computerLogic();
 
 if (x <= 0.33) {
-    computerSelect = "Rock";
-} else if (0.33 < x <= 0.67) {
-    computerSelect = "Paper";
-} else {
-    computerSelect = "Scissors";
+    computerSelect = "rock";
+}else if (0.33 < x <= 0.67) {
+    computerSelect = "paper";
+}else {
+    computerSelect = "scissors";
 }
 //Computer Rock Paper Scissors RNG
 
+
+let user = prompt("Please choose rock, paper, or scissors to play.");
+let userSelect = user.toLowerCase();
+//User input is not case sensitive
+
+function round (userSelect, computerSelect) {
+    if (userSelect == "rock") {
+        if(computerSelect == "rock") {
+            roundOutcome = "Rock hits rock, nothing happens!";
+        }else if(computerSelect == "paper") {
+            roundOutcome = "Paper beats rock, computer wins!";
+        }else {
+            roundOutcome = "Rock beats scissors, you win!";
+        }
+    }else if (userSelect == "paper") {
+            if(computerSelect == "rock") {
+                roundOutcome = "Paper beats rock, you win!";
+            }else if(computerSelect == "paper") {
+                roundOutcome = "Paper hits paper, nothing happens!";
+            }else {
+                roundOutcome = "Scissors cuts paper, computer wins!";
+            }
+    }else {
+            if(computerSelect == "rock") {
+                roundOutcome = "Rock beats scissors, computer wins!!";
+            }else if(computerSelect == "paper") {
+                roundOutcome = "Scissors cuts paper, you win!";
+            }else {
+                roundOutcome = "Scissors hits scissors, nothing happens!";
+            }
+        }
+    return roundOutcome;
+}
+//Computer and User play one round of RPS
+
+let roundwinner = round(userSelect, computerSelect);
+
+console.log(userSelect);
 console.log(computerSelect);
+console.log(roundwinner);
